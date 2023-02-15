f5fca5cfd1d4a986b6f6d794473d171f1fd51acb
BimaAdi <bimaadi419@gmail.com>
2023-02-15 09:27:34 +0700

    setup swaggo

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"github.com/BimaAdi/Oauth2AuthorizationServer/docs"
 	"github.com/BimaAdi/Oauth2AuthorizationServer/models"
 	"github.com/BimaAdi/Oauth2AuthorizationServer/routes"
 	"github.com/BimaAdi/Oauth2AuthorizationServer/settings"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title Go Oauth2 Authorization Server
+// @version 1.0
+// @description Oauth2 Authorization server
 func main() {
 	// Initialize environtment variable
 	settings.InitiateSettings(".env")
@@ -14,7 +20,14 @@ func main() {
 	models.Initiate()
 	models.AutoMigrate() // Run Migration
 
-	// run gin server
+	// Initialize gin route
 	routes := routes.GetRoutes()
+
+	// setup swagger
+	docs.SwaggerInfo.BasePath = "/"
+	docs.SwaggerInfo.Host = settings.SERVER_HOST + ":" + settings.SERVER_PORT
+	routes.GET("/docs/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	// run gin server
 	routes.Run(settings.SERVER_HOST + ":" + settings.SERVER_PORT)
 }
